cc8261e22a26c700b0d24cfc403e520e3df731c4
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-12-02 21:04:53 +0100

    how to pass back as a promise?

--- database/queries/GetYearsActiveRange.js
+++ database/queries/GetYearsActiveRange.js
@@ -1,3 +1,6 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 const Artist = require('../models/artist')
 
 /**
@@ -5,4 +8,28 @@ const Artist = require('../models/artist')
  * @return {promise} A promise that resolves with an object
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
-module.exports = () => {}
+module.exports = async () => {
+  // const artists = await Artist.find( {}).sort( {yearsActive: 1} )
+  // console.log( artists )
+
+  const minModel = await Artist.find({})
+    .sort({ yearsActive: 1 })
+    .limit(1)
+  console.log(minModel)
+  const min = minModel[0].yearsActive
+
+  const maxModel = await Artist.find({})
+    .sort({ yearsActive: -1 })
+    .limit(1)
+  console.log(maxModel)
+  const max = maxModel[0].yearsActive
+
+  console.log(min)
+  console.log(max)
+
+  /*
+	return new Promise().then(() => {
+		resolve( { min: 0, max: 14 } )
+	})
+	*/
+}
