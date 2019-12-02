c5a08eb89cb83d70968671c191450c20e2d7c7b0
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-12-03 00:21:43 +0100

    working on this

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -1,3 +1,6 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 const Artist = require('../models/artist')
 
 /**
@@ -5,4 +8,52 @@ const Artist = require('../models/artist')
  * @return {promise} A promise that resolves with an object
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
-module.exports = () => {}
+module.exports = async () => {
+  const minModel = await Artist.find({})
+    .sort({ age: 1 })
+    .limit(1)
+  console.log(minModel)
+  const min = minModel[0].age
+
+  const maxModel = await Artist.find({})
+    .sort({ age: -1 })
+    .limit(1)
+  console.log(maxModel)
+  const max = maxModel[0].age
+
+  console.log(min)
+  console.log(max)
+
+  const minPromise = Artist.find({})
+    .sort({ age: 1 })
+    .limit(1)
+
+  const maxPromise = Artist.find({})
+    .sort({ age: -1 })
+    .limit(1)
+
+  return Promise.all([minPromise, maxPromise]).then(wtf => {
+    console.log(wtf)
+    const minMaxAge = {
+      min: wtf[0][0].age,
+      max: wtf[1][0].age,
+    }
+
+    console.log(minMaxAge)
+
+    return minMaxAge
+
+    /*
+		return {
+			min: wtf[0].age,
+			max: wtf[1].age
+		}
+		*/
+  })
+
+  /*
+	return new Promise().then(() => {
+		resolve( { min: 0, max: 14 } )
+	})
+	*/
+}
