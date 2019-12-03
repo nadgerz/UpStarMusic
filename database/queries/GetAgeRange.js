96e0571542dce16026c6c9be7e2aa47e85a71204
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-12-03 01:32:53 +0100

    pull out into min/max utils

--- database/queries/GetAgeRange.js
+++ database/queries/GetAgeRange.js
@@ -8,21 +8,28 @@ const Artist = require('../models/artist')
  * @return {promise} A promise that resolves with an object
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
-const promise = (order = 1) => {
+const QueryOrder = {
+  ASC: 1,
+  DESC: -1,
+}
+
+const query = (order = QueryOrder.ASC) => {
   return Artist.find({})
     .sort({ age: order })
     .limit(1)
     .then(artists => artists[0].age)
 }
 
-const getAgeRange = async () => {
-  const min = await promise()
-  const max = await promise(-1)
+const getMinMax = async () => {
+  const min = await query()
+  const max = await query(QueryOrder.DESC)
 
   return {
     min,
     max,
   }
 }
+
+const getAgeRange = getMinMax
 
 module.exports = () => getAgeRange()
